e142dd075e875e59cf83200e7cbb4d8494024009
Hyungju Kwon <hyungju.kwon@gmail.com>
2023-09-18 19:49:45 -0400

    404 page bug fixed

--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -2,7 +2,7 @@ import PageTitle from './PageTitle'
 const NotFound = () => {
 	return (
 		<>
-			<PageTitle text='Four Zero Four (404) Not Found' size={5} />
+			<PageTitle size={5}>Four Zero Four (404) Not Found</PageTitle>
 
 			<p className='text-white text-center mb-5'>
 				Ayoye! It's either bands don't have web presence or something went
