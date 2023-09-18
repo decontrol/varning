bc8ef674e3a24a3370600fa2f93f5e5fa5f92afb
Hyungju Kwon <hyungju.kwon@gmail.com>
2023-09-18 17:44:31 -0400

    more tweaks

--- src/components/CarouselButton.tsx
+++ src/components/CarouselButton.tsx
@@ -11,7 +11,7 @@ const CarouselButton = ({
 	direction,
 	hide,
 }: CarouselButtonProps) => {
-	const classes = `carousel-button text-4xl lg:text-7xl text-white bg-red-600 bg-opacity-50 px-4 py-2 absolute top-[50%] translate-y-[-50%] z-2 origin-center hover:bg-red-600 hover:opacity-75 hover:scale-110 active:bg-red-600 active:opacity-75 active:scale-110 active:outline-2 active:outline-white 
+	const classes = `carousel-button text-4xl lg:text-7xl text-white bg-red-600 bg-opacity-50 px-4 py-2 absolute top-[50%] translate-y-[-50%] z-2 origin-center hover:bg-red-600 hover:opacity-85 hover:scale-110 active:bg-red-600 active:opacity-85 active:scale-110 active:outline-2 active:outline-white 
 	${hide ? 'hidden' : 'block'}
 	${direction === 'prev' ? 'left-0' : 'right-0'}`
 	return (
--- src/components/NotFound.tsx
+++ src/components/NotFound.tsx
@@ -5,7 +5,8 @@ const NotFound = () => {
 			<PageTitle text='Four Zero Four (404) Not Found' size={5} />
 
 			<p className='text-white text-center mb-5'>
-				It's either bands don't have web presence or something went wrong!
+				Ayoye! It's either bands don't have web presence or something went
+				wrong!
 			</p>
 
 			<p className='text-3xl text-center'>😳🤪💩🥸🤡</p>
