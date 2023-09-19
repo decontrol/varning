59092df739470e2a105a5b3615a54f3f47529139
Hyungju Kwon <hyungju.kwon@gmail.com>
2023-09-18 20:19:50 -0400

    things to do added

--- src/components/ThingsToDo.tsx
+++ src/components/ThingsToDo.tsx
@@ -7,29 +7,71 @@ const ThingsToDo = () => {
 			<PageTitle size={5}>Here are things to do in Montreal</PageTitle>
 			<h3 className={headerClasses}>Alcohol</h3>
 			<ul className='text-white'>
-				<li>Cheval Blanc (809 Ontario St E)</li>
-				<li>L'Espace Public (3632 Ontario E)</li>
-				<li>Dieu du ciel (29 Avenue Laurier)</li>
-				<li>L'Amère A Boire (2049 St-Denis)</li>
+				<li>
+					<strong>Cheval Blanc</strong> (809 Ontario St E)
+				</li>
+				<li>
+					<strong>L'Espace Public</strong> (3632 Ontario E)
+				</li>
+				<li>
+					<strong>Dieu du ciel</strong> (29 Avenue Laurier)
+				</li>
+				<li>
+					<strong>L'Amère A Boire</strong> (2049 St-Denis)
+				</li>
 			</ul>
 
 			<h3 className={headerClasses}>Food/Poutine</h3>
 			<ul className='text-white'>
-				<li>Lafleuer (2620 St-denis / 2120 Ontario E)</li>
-				<li>Boustan (19 Ste-Catherine E / 3417 Ontario St E)</li>
-				<li>Aux vivres (4631 St Laurent)</li>
-				<li>La Panthère Verte (1735 St-Denis)</li>
-				<li>Yuan (2115 St-Denis)</li>
+				<li>
+					<strong>Shawarmaz</strong> (1206 Peel St)
+				</li>
+				<li>
+					<strong>Le Trèfle Hochelaga</strong> (3971 Ontario E)
+				</li>
+				<li>
+					<strong>Resto-Bar Le Pick-Up</strong> (3739 Ontario St E)
+				</li>
+				<li>
+					<strong>Lafleuer</strong> (2620 St-denis / 2120 Ontario E)
+				</li>
+				<li>
+					<strong>Boustan</strong> (19 Ste-Catherine E / 3417 Ontario St E)
+				</li>
+				<li>
+					<strong>Aux Vivres</strong> (4631 St Laurent)
+				</li>
+				<li>
+					<strong>La Panthère Verte</strong> (1735 St-Denis)
+				</li>
+				<li>
+					<strong>Yuan</strong> (2115 St-Denis)
+				</li>
 			</ul>
 
 			<h3 className={headerClasses}>Records</h3>
 			<ul className='text-white'>
-				<li>Sonik (4050 Berri)</li>
-				<li>Cheap Thrills (2044 Metcalfe)</li>
-				<li>Le Pick-up (169 Pins Ave E)</li>
-				<li>Pump Up the Volume (1359 Ontario E)</li>
-				<li>Aux 33 tours (1373 Mont-Royal Ave E)</li>
-				<li>Sound Central (486 Coloniale Ave)</li>
+				<li>
+					<strong>Sonik</strong> (4050 Berri)
+				</li>
+				<li>
+					<strong>Cheap Thrills</strong> (2044 Metcalfe)
+				</li>
+				<li>
+					<strong>Beatnick Records</strong> (3770 Saint Denis)
+				</li>
+				<li>
+					<strong>Le Pick-up / LeBackstore</strong> (169 Pins Ave E)
+				</li>
+				<li>
+					<strong>Pump Up the Volume</strong> (1359 Ontario E)
+				</li>
+				<li>
+					<strong>Sound Central</strong> (486 Coloniale Ave)
+				</li>
+				<li>
+					<strong>Aux 33 tours</strong> (1373 Mont-Royal Ave E)
+				</li>
 			</ul>
 		</>
 	)
