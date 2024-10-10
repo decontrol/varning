443c6aeca716ab673047761963f1254009a21301
Hyungju Kwon <hyungju.kwon@gmail.com>
2024-10-10 17:03:26 -0400

    aftwerparty flyer

--- src/data/images.ts
+++ src/data/images.ts
@@ -5,7 +5,7 @@ import Img4 from '../assets/images/Friday-Main.jpg';
 import Img5 from '../assets/images/Saturday-Market.jpg';
 import Img6 from '../assets/images/Saturday-Matinee.jpg';
 import Img7 from '../assets/images/Saturday-Main.jpg';
-import Img8 from '../assets/images/Saturday-Main.jpg';
+import Img8 from '../assets/images/Saturday-Afterparty.jpg';
 
 import Logo from '../assets/images/app/varning-header.png';
 import LogoWide from '../assets/images/app/varning-header-wide.png';
